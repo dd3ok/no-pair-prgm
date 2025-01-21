f91b56981ebc7cedfc57ed4751b15023ad288b4f
dd3ok <hwick@nate.com>
2025-01-21 09:26:54 +0900

    init

---
+++ src/main/java/com/blog/nopairprgm/NoPairPrgmApplication.java
@@ -0,0 +1,13 @@
+package com.blog.nopairprgm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoPairPrgmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoPairPrgmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blog/nopairprgm/NoPairPrgmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.nopairprgm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoPairPrgmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
