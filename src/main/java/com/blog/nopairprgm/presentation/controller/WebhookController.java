277a3fe7db6ff7f817650d835d20d88723a5c4bf
dd3ok <hwick@nate.com>
2025-01-21 11:54:01 +0900

    pr 리뷰 테스트

--- src/main/java/com/blog/nopairprgm/presentation/controller/WebhookController.java
+++ src/main/java/com/blog/nopairprgm/presentation/controller/WebhookController.java
@@ -3,6 +3,7 @@ package com.blog.nopairprgm.presentation.controller;
 import com.blog.nopairprgm.application.service.PullRequestService;
 import com.blog.nopairprgm.presentation.dto.WebhookRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,16 +11,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class WebhookController {
     private final PullRequestService pullRequestService;
-    
-    @PostMapping("/api/webhook/github")
+
+    @PostMapping("/github")
     public ResponseEntity<Void> handlePullRequestWebhook(@RequestBody WebhookRequest request) {
+        log.info("Received webhook - PR ID: {}, Number: {}, Action: {}",
+                request.getPullRequest().getId(),
+                request.getPullRequest().getNumber(),  // number 필드 추가 필요
+                request.getAction()
+        );
+        log.info("Repository: {}", request.getRepository().getFullName());
+
         if ("opened".equals(request.getAction())) {
             pullRequestService.processPullRequest(request);
         }
-        // 진입 테스트
-        String string = "";
         return ResponseEntity.ok().build();
     }
+
 }
